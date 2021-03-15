e7281688016366372d31205bb7efe55538aefd88
rotciv <victorfage@gmail.com>
2021-03-15 20:40:09 +0100

    fix

--- contracts/CryptoChemical.sol
+++ contracts/CryptoChemical.sol
@@ -4,13 +4,11 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./interfaces/IManager.sol";
-
 
 contract CryptoChemical is ERC20("Crypto Chemical Energy", "CCE"), ERC1155, Ownable {
-    event SetManager(IManager _manager);
+    event SetManager(address _manager);
 
-    IManager public manager;
+    address public manager;
 
     constructor (string memory _uri) ERC1155(_uri) { }
 
@@ -18,8 +16,8 @@ contract CryptoChemical is ERC20("Crypto Chemical Energy", "CCE"), ERC1155, Owna
         _setURI(_uri);
     }
 
-    function setManager(IManager _manager) external onlyOwner {
-        require(_manager != IManager(0), "setManager: Manager 0x0 is not valid");
+    function setManager(address _manager) external onlyOwner {
+        require(_manager != address(0), "setManager: Manager 0x0 is not valid");
 
         manager = _manager;
         emit SetManager(_manager);
@@ -35,7 +33,7 @@ contract CryptoChemical is ERC20("Crypto Chemical Energy", "CCE"), ERC1155, Owna
         uint256 _amount,
         bytes memory _data
     ) external {
-        require(msg.sender == address(manager), "mint: Only the manager can mint");
+        require(msg.sender == manager, "mint: Only the manager can mint");
 
         _mint(_account, _id, _amount, _data);
     }
@@ -46,7 +44,7 @@ contract CryptoChemical is ERC20("Crypto Chemical Energy", "CCE"), ERC1155, Owna
         uint256[] memory _amounts,
         bytes memory _data
     ) external {
-        require(msg.sender == address(manager), "mintBatch: Only the manager can mint");
+        require(msg.sender == manager, "mintBatch: Only the manager can mint");
 
         _mintBatch(_account, _ids, _amounts, _data);
     }
